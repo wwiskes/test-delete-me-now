ac81dc119183ee6ca77b6e241658800d4d2bd5b2
William Wiskes <83665599+wwiskes@users.noreply.github.com>
2023-12-25 19:33:45 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
+        </p> all hail the loon king
         <a
           className="App-link"
           href="https://reactjs.org"
